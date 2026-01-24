1477151724014a408f6512b578231a1bff76a55c
Vadym <vadymkravtcsov@gmail.com>
2026-01-24 20:39:23 +0200

    feat: definet and established paths to representations templates

--- Django-Basics/django_project/notes/urls.py
+++ Django-Basics/django_project/notes/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from notes.views import NoteDetailView, NoteListView, NoteCreateView
+from notes.views import NoteDetailView, NoteListView, NoteCreateView, NoteUpdateView, NoteDeleteView
 
 urlpatterns = [
     
 	path("", NoteListView.as_view(), name="note-list"),
-	path("<int:pk>/", NoteDetailView.as_view(), name="note-detail"),
 	path("create/", NoteCreateView.as_view(), name="note-create"),
+	path("<int:pk>/delail/", NoteDetailView.as_view(), name="note-detail"),
+    path("<int:pk>/edit/", NoteUpdateView.as_view(), name="note-edit"),
+    path("<int:pk>/delete/", NoteDeleteView.as_view(), name="note-delete")
+    
 ]
-
