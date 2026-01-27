a0275795984e7e12e97b992217c53f405a4ddcaf
Vadym <vadymkravtcsov@gmail.com>
2026-01-27 14:42:26 +0200

    style: removing spaces.

--- Django-Basics/django_project/notes/forms.py
+++ Django-Basics/django_project/notes/forms.py
@@ -8,6 +8,6 @@ class NoteForm(forms.ModelForm):
         model = Note
         fields = ['title', 'body']
         widgets = {
-            "title": forms.TextInput(attrs={"class": "form-control "}),
+            "title": forms.TextInput(attrs={"class": "form-control"}),
             "body": forms.Textarea(attrs={"class": "form-control"}),
         }
