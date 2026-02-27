f9e514daffd088e02b906f7548667cf61dff9226
Vadym <vadymkravtcsov@gmail.com>
2026-02-27 13:34:14 +0200

    feat: update CreateUserForm to include first and last name fields, remove password validation

--- Django-Authen-Authz/django_project/accounts/forms.py
+++ Django-Authen-Authz/django_project/accounts/forms.py
@@ -15,7 +15,7 @@ class CreateUserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['email', 'password1', 'password2']
+        fields = ['email', 'first_name', 'last_name']
 
 
     def clean_password2(self):
@@ -32,12 +32,6 @@ class CreateUserForm(forms.ModelForm):
         if commit:
             user.save()
         return user
-
-    def _post_clean(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Password don't match")
 
 class ChangeUserForm(forms.ModelForm):
     password = ReadOnlyPasswordHashField()
