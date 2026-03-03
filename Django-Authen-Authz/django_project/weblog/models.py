035ff1ac12e1007c46010c580c5321af16929187
Vadym <vadymkravtcsov@gmail.com>
2026-03-03 08:18:49 +0200

    refactor: improve code organization and fix typo in article field of Post model

--- Django-Authen-Authz/django_project/weblog/models.py
+++ Django-Authen-Authz/django_project/weblog/models.py
@@ -3,7 +3,9 @@ from django.db.models.functions import Lower
 from django.conf import settings
 from cloudinary.models import CloudinaryField
 
-
+# >---------------------------------------<
+# (Tag model) ----------------------------<
+# >---------------------------------------<
 class Tag(models.Model):
     name = models.CharField(verbose_name="Tag", max_length=10, unique=True)
 
@@ -18,7 +20,9 @@ class Tag(models.Model):
         verbose_name = "Tag"
         verbose_name_plural = "Tags"
 
-
+# >---------------------------------------<
+# (Post model) ---------------------------<
+# >---------------------------------------<
 class Post(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -26,20 +30,20 @@ class Post(models.Model):
         related_name="posts",
     )
     title = models.CharField(verbose_name="Title", max_length=100)
-    article = models.TextField(verbose_name="Articte")
+    article = models.TextField(verbose_name="Article")
     created_at = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag)
+    image = CloudinaryField(verbose_name="image", blank=True)
 
     class Meta:
         ordering = ["-created_at"]
 
-
+# >---------------------------------------<
+# (Comment model) ------------------------<
+# >---------------------------------------<
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="Comments")
     author = models.CharField(verbose_name="Author", max_length=100)
     text = models.TextField()
 
 
-class Image(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="Images")
-    image = CloudinaryField("Image")
