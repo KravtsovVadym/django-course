13e6bd40687f716f14f2b511cceaf5d3dbda78a0
Vadym <vadymkravtcsov@gmail.com>
2026-03-06 17:40:47 +0200

    docs: add comments and format admin.py for clarity

--- Django-Authen-Authz/django_project/accounts/admin.py
+++ Django-Authen-Authz/django_project/accounts/admin.py
@@ -1,3 +1,7 @@
+# >---------------------------------------<
+# (Django Admin Configuration) -----------<
+# >---------------------------------------<
+
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseAdmin
@@ -7,47 +11,42 @@ from accounts.forms import CreateUserForm, ChangeUserForm
 
 @admin.register(User)
 class UserAdmin(BaseAdmin):
+    # Custom admin interface for User model
+    # Forms for editing and creating users
     form = ChangeUserForm
     add_form = CreateUserForm
 
-    list_display = [
-        'email',
-        'first_name',
-        'last_name',
-        'created_at',
-        'is_superuser'
-    ]
-    list_filter = ['is_superuser']
+    # Display configuration
+    list_display = ["email", "first_name", "last_name", "created_at", "is_superuser"]
+    list_filter = ["is_superuser"]
+    
+    # Edit form layout
     fieldsets = [
-        (
-            None,
-            {
-                'fields': [
-                    'email',
-                    'first_name',
-                    'last_name',
-                    'is_superuser'
-                ]
-            }
-        ),
+        (None, {"fields": ["email", "first_name", "last_name", "is_superuser"]}),
     ]
+    
+    # Add form layout
     add_fieldsets = [
         (
             None,
             {
-                'fields': [
-                    'email',
-                    'first_name',
-                    'last_name',
-                    'password1',
-                    'password2',
-                    'is_superuser'
+                "fields": [
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_superuser",
                 ]
-            }
+            },
         ),
     ]
-    search_fields = ['email']
-    ordering = ['email']
+    
+    # Search and ordering
+    search_fields = ["email"]
+    ordering = ["email"]
     filter_horizontal = ()
 
+
+# Remove default Group model from admin
 admin.site.unregister(Group)
